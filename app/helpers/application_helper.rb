6d4421c74fe07ab8b7b0cef67e31bf17af11bcaa
Anton <antonz@svitla.com>
2012-05-28 22:21:39 +0300

    added posts to categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,19 @@ module ApplicationHelper
     return html unless subcategories
     html << "<ul>"
     subcategories.each do |category|
+      check_for_posts(category, html)
       html << "<li><span class='folder'>#{category.name}</span>"
       subcategory_for(category, html)
       html << "</li>"
     end
     html << "</ul>"
+  end
+
+  def check_for_posts(category, html = "")
+    if category.posts.any?
+      category.posts.each do |post|
+        html << "<li><span class='file'>#{post.title}</span></li>"
+      end
+    end
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   attr_accessible :name, :subcategory_id
   belongs_to :user
+  has_many :posts
   scope :first_level, where(:subcategory_id => nil)
   scope :subcategory_for, lambda { |category_id| where("subcategory_id = ?", category_id ) }
 end
