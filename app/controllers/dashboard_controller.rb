759beaf89651af676e9d7e68aa901f2e977be5e1
Anton Zimin <antonz@svitla.com>
2012-05-28 17:38:54 +0300

    implemented categories tree

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+  before_filter :authenticate_user!
   def index
     @categories = current_user.categories
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
-  
+  def subcategory_for(category, html = "")
+    subcategories = @categories.subcategory_for(category)
+    return html unless subcategories
+    html << "<ul>"
+    subcategories.each do |category|
+      html << "<li><span class='folder'>#{category.name}</span></li>"
+      subcategory_for(category, html)
+    end
+    html << "</ul>"
+  end
 end
