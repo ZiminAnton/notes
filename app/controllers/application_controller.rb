20e62dc7e61b9ccaf012de0f21b1a5e3532c5fdb
Anton <antonz@svitla.com>
2012-05-28 22:25:30 +0300

    added before_filter for categories in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :get_user
+  before_filter :get_user, :get_categories_for_login_user
 
   def get_user
     @user = current_user
+  end
+
+  def get_categories_for_login_user
+    @categories = @user.categories if @user
   end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
   def index
-    @categories = current_user.categories
+    
   end
 end
